38ea9534c33ded9d11df7dcd06ef566d9490894b
Aimi <minervavine@gmail.com>
2019-12-14 22:53:10 +1000

    Fix name Undefined bug

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 
 // This is out apps base route. Any request to localhost:3000/
 // is handled here
-app.get('/', function(req, res) {
+app.get('/', async function(req, res) {
 
-	// Start our reponse string
-	var response = 'Hello ';
+	// Start our response string
+	let response = 'Hello ';
 
 	// Lets fetch our username for somewhere
-	getUserName(function(err, name) {
+	await getUserName(function(err, name) {
 		if (err) {
 			return res.status(500).send('Something is not right!')
 		}
 		// Join our strings
-		response = response + name;
+		response += name;
+		res.send(response);
 	});
 
 	// Return response to client
-	res.send(response)
+
 })
 
 
 // This function will return our username to print
-function getUserName(callback) {
+async function getUserName(callback) {
 	// Wrapping it in a timeout 
 	// to simulate if this is a database request
 	setTimeout(function() {
@@ -34,4 +35,4 @@ function getUserName(callback) {
 }
 
 // Port for server to start and listen on
-app.listen(3000, () => console.log('Buggy app listening on port 3000!'))
+app.listen(3000, () => console.log('Buggy app listening on port 3000!'));
